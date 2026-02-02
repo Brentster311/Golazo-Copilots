c9b4abd4ed145dcc0e9485c465c1955b7130ed14
Brent Jensen <brentj@microsoft.com>
2026-02-02 15:33:10 -0800

    GCP-0004: Workflow Status - gcp_status tool with comprehensive state display

--- Golazo-Copilotv2/golazo_copilot/src/golazo_copilot/tools/__init__.py
+++ Golazo-Copilotv2/golazo_copilot/src/golazo_copilot/tools/__init__.py
@@ -3,5 +3,6 @@
 from .gcp_init import gcp_init
 from .gcp_transition import gcp_transition
 from .gcp_mark import gcp_mark_dor, gcp_mark_dod
+from .gcp_status import gcp_status
 
-__all__ = ["gcp_init", "gcp_transition", "gcp_mark_dor", "gcp_mark_dod"]
+__all__ = ["gcp_init", "gcp_transition", "gcp_mark_dor", "gcp_mark_dod", "gcp_status"]
