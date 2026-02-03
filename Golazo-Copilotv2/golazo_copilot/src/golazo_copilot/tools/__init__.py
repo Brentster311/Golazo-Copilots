3bada9258093254f26dcc0152d6087c7058216c2
Brent Jensen <brentj@microsoft.com>
2026-02-02 21:44:10 -0800

    feat(GCP-0005): Add gcp_consent tool for deviation tracking - v0.3.0

--- Golazo-Copilotv2/golazo_copilot/src/golazo_copilot/tools/__init__.py
+++ Golazo-Copilotv2/golazo_copilot/src/golazo_copilot/tools/__init__.py
@@ -5,5 +5,6 @@ from .gcp_transition import gcp_transition
 from .gcp_mark import gcp_mark_dor, gcp_mark_dod
 from .gcp_status import gcp_status
 from .gcp_bootstrap import gcp_bootstrap
+from .gcp_consent import gcp_consent
 
-__all__ = ["gcp_create_workitem", "gcp_transition", "gcp_mark_dor", "gcp_mark_dod", "gcp_status", "gcp_bootstrap"]
+__all__ = ["gcp_create_workitem", "gcp_transition", "gcp_mark_dor", "gcp_mark_dod", "gcp_status", "gcp_bootstrap", "gcp_consent"]
